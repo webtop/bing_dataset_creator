4df3d55e7623b6474cdf1ae7b3eaa8c4b4fa5fee
Paul Allsopp <pallsopp@gmail.com>
2022-08-25 11:27:15 -0700

    Adding comments

--- mover.php
+++ mover.php
@@ -6,10 +6,10 @@ $fi = new FilesystemIterator($trainPath, FilesystemIterator::SKIP_DOTS);
 
 foreach ($fi as $key => $fileInfo) {
     $fi2 = new FilesystemIterator($key, FilesystemIterator::KEY_AS_PATHNAME);
-    $size = (int)(iterator_count($fi2) * 0.2) ;
+    $size = (int)(iterator_count($fi2) * 0.2) ; // Calculate 20% of files
     $count = 0;
     foreach ($fi2 as $subKey => $subFileInfo) {
-        if ($count == $size) break;
+        if ($count == $size) break; // Once we've moved 20% of the files, quit
         print "Will move " . $subKey . " to " . str_replace('/train/', '/test/', $subKey) . "\n";
         rename($subKey, str_replace('/train/', '/test/', $subKey));
         $count ++;
